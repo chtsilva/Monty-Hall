f1a6a71c2ca2b5ecc1700e4142dae79101649686
Carlos Henrique <chtsilva104@gmail.com>
2021-07-15 11:42:16 -0300

    Alterações

--- components/Porta.jsx
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/Porta.module.css'
-
-export default function Porta(props){
-  return(
-    <div className={styles.porta}>
-      <div className={styles.numero}>3</div>
-    </div>
-  )
-}
