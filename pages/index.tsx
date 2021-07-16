968e52b70b2fceb13dcb3b19ef7b99ccc72539e0
Carlos Henrique <chtsilva104@gmail.com>
2021-07-16 10:41:48 -0300

    Finalização do Projeto

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,13 @@ import styles from '../styles/Formulario.module.css'
 import Cartao from "../components/Cartao";
 
 import Link from 'next/link'
+import EntradaNumerica from '../components/EntradaNumerica';
+import { useState } from 'react';
 
 export default function Formulario() {
+  const [qtdePortas, setQtdePortas] = useState(3)
+  const [comPresente, setComPresente] = useState(1)
+
   return (
     <div className={styles.formulario}>
       <div>
@@ -11,12 +16,16 @@ export default function Formulario() {
           <h1>Monty Hall</h1>
         </Cartao>
         
-        <Cartao ></Cartao>
+        <Cartao >
+          <EntradaNumerica text="Qtde Portas?" value={qtdePortas} onChange={novaQtde => setQtdePortas(novaQtde)} />
+        </Cartao>
       </div>
       <div>
-        <Cartao></Cartao>
+        <Cartao>
+        <EntradaNumerica text="Porta com presente?" value={comPresente} onChange={novaQtde => setComPresente(novaQtde)} />
+        </Cartao>
         <Cartao bgcolor="#28a085">
-          <Link href={`/jogo/4/2`}>
+          <Link href={`/jogo/${qtdePortas}/${comPresente}`} passHref>
             <h2  className={styles.link}>Iniciar</h2>
           </Link>
         </Cartao>
--- pages/jogo/[portas]/[temPresente].tsx
+++ pages/jogo/[portas]/[temPresente].tsx
@@ -9,8 +9,19 @@ import styles from '../../../styles/Jogo.module.css'
 export default function Jogo() {
   const router = useRouter()
   
+  const [valido, setValido] = React.useState(false)
   const [portas, setPortas] = React.useState([])
 
+  React.useEffect(() =>{
+    const portas = +router.query.portas
+    const temPresente = +router.query.temPresente
+
+    const qtdePortasValidas = portas >= 3 && portas <= 100 
+    const temPresenteValido = temPresente >= 1 &&  temPresente <= portas 
+
+    setValido(qtdePortasValidas && temPresenteValido)
+  }, [portas, router.query.portas,router.query.temPresente] )
+  
   React.useEffect(() =>{
     const portas = +router.query.portas
     const temPresente = +router.query.temPresente
@@ -29,10 +40,13 @@ export default function Jogo() {
   return (
     <div id={styles.jogo}>
       <div className={styles.portas}>
-        {renderizarPortas()}
+        { valido ? 
+        renderizarPortas() :
+          <h1>Valores inválidos</h1>
+      }
       </div>
       <div className={styles.botoes}>
-        <Link href="/" >
+        <Link href="/" passHref>
           <button>Reiniciar</button>
         </Link>
       </div>
